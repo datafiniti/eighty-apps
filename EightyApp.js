9cd018ef3140806266e3852e777e6b591af65705
veleary <veleary@gmail.com>
2014-03-25 19:48:50 -0500

    fix make link method bug causing duplicate slashes

--- EightyApp.js
+++ EightyApp.js
@@ -1,117 +1,119 @@
 var EightyAppBase = function() {
-	var authStatus;
+    var authStatus;
 
-	var initialize = function() {
-		authStatus = false;
-	};
+    var initialize = function() {
+        authStatus = false;
+    };
 
-	this.processDocument = function(html, url, headers, status, jQuery) {
-	};
+    this.processDocument = function(html, url, headers, status, jQuery) {};
 
-	this.parseLinks = function(html, url, headers, status, jQuery) {
-	};
+    this.parseLinks = function(html, url, headers, status, jQuery) {};
 
-	this.parseJSON = function(text){
-		return JSON.parse(text);
-	}
+    this.parseJSON = function(text) {
+        return JSON.parse(text);
+    }
 
-	this.parseHtml = function(text, $) {
-		text = text.replace(/(<img)\+*?/g, "<img80");
-		return $(text);
-	}
+    this.parseHtml = function(text, $) {
+        text = text.replace(/(<img)\+*?/g, "<img80");
+        return $(text);
+    }
 
-	this.getPlainText = function(text) {
-		text = text
-			.replace(/[^a-z0-9\s.'-:]/gi, '') // remove all characters not a-z, 0-9, and certain punctuation, ignoring case
-			.replace(/\s{2,}/g, ' ') // replace any two whitespace characters next to each other with a single space
-			.replace(/\s/g, ' '); // replace all whitespace characters (\t,\n,\r, ) with space
+    this.getPlainText = function(text) {
+        text = text
+            .replace(/[^a-z0-9\s.'-:]/gi, '') // remove all characters not a-z, 0-9, and certain punctuation, ignoring case
+        .replace(/\s{2,}/g, ' ') // replace any two whitespace characters next to each other with a single space
+        .replace(/\s/g, ' '); // replace all whitespace characters (\t,\n,\r, ) with space
 
-		// trim
-		return text.replace( /^\s\s*/, "" ).replace( /\s\s*$/, "" );
-	};
+        // trim
+        return text.replace(/^\s\s*/, "").replace(/\s\s*$/, "");
+    };
 
-	this.formatDate = function(date) {
-		// yyyy-MM-ddTHH:mm:ssZ
-		date = new Date(date);
-		return date.getUTCFullYear() + '-' +
-			(date.getUTCMonth()+1<10? '0'+(date.getUTCMonth()+1):''+(date.getUTCMonth()+1)) + '-' +
-			(date.getUTCDate()<10? '0'+date.getUTCDate():''+date.getUTCDate()) + '-' +
-			'T' +
-			(date.getUTCHours()<10? '0'+date.getUTCHours():''+date.getUTCHours()) + ':' +
-			(date.getUTCMinutes()<10? '0'+date.getUTCMinutes():''+date.getUTCMinutes()) + ':' +
-			(date.getUTCSeconds()<10? '0'+date.getUTCSeconds():''+date.getUTCSeconds()) +
-			'Z';
-	}
+    this.formatDate = function(date) {
+        // yyyy-MM-ddTHH:mm:ssZ
+        date = new Date(date);
+        return date.getUTCFullYear() + '-' +
+            (date.getUTCMonth() + 1 < 10 ? '0' + (date.getUTCMonth() + 1) : '' + (date.getUTCMonth() + 1)) + '-' +
+            (date.getUTCDate() < 10 ? '0' + date.getUTCDate() : '' + date.getUTCDate()) + '-' +
+            'T' +
+            (date.getUTCHours() < 10 ? '0' + date.getUTCHours() : '' + date.getUTCHours()) + ':' +
+            (date.getUTCMinutes() < 10 ? '0' + date.getUTCMinutes() : '' + date.getUTCMinutes()) + ':' +
+            (date.getUTCSeconds() < 10 ? '0' + date.getUTCSeconds() : '' + date.getUTCSeconds()) +
+            'Z';
+    }
 
-	this.removeTag = function(text){
-		return text.replace(/<.*?>/g, "");
-	}
+    this.removeTag = function(text) {
+        return text.replace(/<.*?>/g, "");
+    }
 
-	this.getFirstMatch = function(text, regexp){
-		regexp = regexp		
-		var matchedGroup = regexp.exec(text);
-		if (matchedGroup !== null && matchedGroup !== undefined){
-			return matchedGroup[1].trim();
-		}
-	}
+    this.getFirstMatch = function(text, regexp) {
+        regexp = regexp
+        var matchedGroup = regexp.exec(text);
+        if (matchedGroup !== null && matchedGroup !== undefined) {
+            return matchedGroup[1].trim();
+        }
+    }
 
-	this.makeLink = function(url, link) {
+    this.makeLink = function(url, link) {
 
-		try {
-			// gets the host from the url
-			var host = url.match(/^http[s]?:\/\/[^/]+/);
-			host = host ? host[0] : null;
+        try {
+            // gets the host from the url
+            var host = url.match(/^http[s]?:\/\/[^/]+/);
+            host = host ? host[0] : null;
 
-			// checks if the link already has a host
-			var linkHost = link.match(/^http[s]?:\/\/[^/]+/);
-			if (linkHost == null) {
-				// returns the link with the host added
-				if (url.substr(url.length - 1) != '/' || link.indexOf(0) != '/') {
-					return host + '/' + link;
-				} else {
-					return host + link;
-				}
-			}
-		} catch(e) {
-			// returns the original link
-			return link;
-		}
-	}
+            // checks if the link already has a host
+            var linkHost = link.match(/^http[s]?:\/\/[^/]+/);
+            if (linkHost == null) {
+                // returns the link with the host added
+                if (link.match(/^\//g)) {
+                    return host + link;
+                } else {
+                    return host + '/' + link;
+                }
+            }
+        } catch (e) {
+            // returns the original link
+            return link;
+        }
+    }
 
-	// eliminateDuplicates code borrowed from: http://dreaminginjavascript.wordpress.com/2008/08/22/eliminating-duplicates/
-	this.eliminateDuplicates = function (arr) {
-		var i;
-		var len=arr.length;
-		var out=[];
-		var obj={};
+    // eliminateDuplicates code borrowed from: http://dreaminginjavascript.wordpress.com/2008/08/22/eliminating-duplicates/
+    this.eliminateDuplicates = function(arr) {
+        var i;
+        var len = arr.length;
+        var out = [];
+        var obj = {};
 
-		for (i=0;i<len;i++) {
-			if (!obj[arr[i]]) {
-				obj[arr[i]]={};
-				out.push(arr[i]);
-			}
-		}
-	  return out;
-	}
+        for (i = 0; i < len; i++) {
+            if (!obj[arr[i]]) {
+                obj[arr[i]] = {};
+                out.push(arr[i]);
+            }
+        }
+        return out;
+    }
 
-	// The following is included to make this testable with node.js
-	try { if(module != null) { var $ = this.$ = ""; } } catch(e) {}
-	this.processTest = function(html, url, headers, status) {
-		var app = this;
-		var env = require('jsdom').env;
-		env(html, function (errors, window) {
-			$ = this.$ = require('jquery')(window);
-			var result = app.processDocument(html, url, headers, status, $);
-			console.log(result);
-			var links = app.parseLinks(html, url, headers, status, $);
-			console.log(links);
-		});
-	}
+    // The following is included to make this testable with node.js
+    try {
+        if (module != null) {
+            var $ = this.$ = "";
+        }
+    } catch (e) {}
+    this.processTest = function(html, url, headers, status) {
+        var app = this;
+        var env = require('jsdom').env;
+        env(html, function(errors, window) {
+            $ = this.$ = require('jquery')(window);
+            var result = app.processDocument(html, url, headers, status, $);
+            console.log(result);
+            var links = app.parseLinks(html, url, headers, status, $);
+            console.log(links);
+        });
+    }
 };
 
 try {
-	// Testing
-	module.exports = EightyAppBase;
-} catch(e) {
-	// Production
+    // Testing
+    module.exports = EightyAppBase;
+} catch (e) {
+    // Production
 }
