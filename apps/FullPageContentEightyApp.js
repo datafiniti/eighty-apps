7bce6937d9026e5ef44df54c2ce0f8682ec8b59c
shiondev <shion@datafiniti.net>
2014-03-26 17:43:54 +0000

    new 80apps

---
+++ apps/FullPageContentEightyApp.js
@@ -0,0 +1,34 @@
+var EightyApp = function() {
+	this.processDocument = function(html, url, headers, status, jQuery) {
+		return html;
+	}
+
+	this.parseLinks = function(html, url, headers, status, jQuery) {
+		var app = this;
+		var $ = jQuery;
+		var $html = app.parseHtml(html, $);
+		var links = [];
+
+		// gets all links in the html document
+		$html.find('a').each(function(i, obj) {
+			var link = app.makeLink(url, $(this).attr('href'));
+			if(link != null) {
+				links.push(link);
+			}
+		});
+
+		return links;
+	}
+}
+
+try {
+	// Testing
+	module.exports = function(EightyAppBase) {
+		EightyApp.prototype = new EightyAppBase();
+		return new EightyApp();
+	}
+} catch(e) {
+	// Production
+	console.log("Eighty app exists.");
+	EightyApp.prototype = new EightyAppBase();
+}
