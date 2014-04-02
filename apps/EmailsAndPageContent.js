6e0f4c058ebab4a12ac1f89625a44755df1fb124
shiondev <shion@datafiniti.net>
2014-04-02 01:56:01 +0000

    adding emailsandpagecontent 80app

---
+++ apps/EmailsAndPageContent.js
@@ -0,0 +1,53 @@
+// Returns a list of email and full page content
+
+var EightyApp = function() {
+	this.processDocument = function(html, url, headers, status, jQuery) {
+		var app = this;
+		$ = jQuery;
+		var $html = app.parseHtml(html, $);
+		var object = {};
+
+		// Get crawl date
+		object.dateCrawled = app.formatDate(Date.now());
+
+		// Get emails
+		var emailList = [];
+		emailList = html.match(/[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9]+)*(\.[a-z]{2,})/gi);
+		object.emailList = emailList;
+
+		// Get page content
+		object.html = html;
+
+		return JSON.stringify(object);
+	}
+
+	this.parseLinks = function(html, url, headers, status, jQuery) {
+		var app = this;
+		var $ = jQuery;
+		var $html = app.parseHtml(html, $);
+		var links = [];
+
+		// gets all links in the html document
+		$html.find('a').each(function(i, obj) {
+			var link = app.makeLink(url, $(this).attr('href'));
+
+			if(link != null) {
+				links.push(link);
+			}
+		});
+
+		return links;
+	}
+}
+
+try {
+	// Testing
+	module.exports = function(EightyAppBase) {
+		EightyApp.prototype = new EightyAppBase();
+		return new EightyApp();
+	}
+} catch(e) {
+	// Production
+	console.log("Eighty app exists.");
+	EightyApp.prototype = new EightyAppBase();
+}
