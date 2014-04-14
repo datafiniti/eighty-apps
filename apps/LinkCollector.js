674183077a00eab4c54eb5e26f3ff6600741d30a
shiondev <shion@datafiniti.net>
2014-04-14 21:06:09 +0000

    adding LinkCollector

---
+++ apps/LinkCollector.js
@@ -0,0 +1,54 @@
+// This 80app returns all links found on a page
+
+var EightyApp = function() {
+	this.processDocument = function(html, url, headers, status, jQuery) {
+                var app = this;
+                var $ = jQuery;
+                var $html = app.parseHtml(html, $);
+		var object = {};
+
+                // gets all links in the html document
+                var links = [];
+                $html.find('a').each(function(i, obj) {
+                        // console.log($(this).attr('href'));
+                        var link = app.makeLink(url, $(this).attr('href'));
+
+                        if(link != null) {
+				links.push(link);
+                        }
+                });
+		object.links = links;
+
+                return JSON.stringify(object);
+	}
+
+	this.parseLinks = function(html, url, headers, status, jQuery) {
+		var app = this;
+		var $ = jQuery;
+		var $html = app.parseHtml(html, $);
+		var links = [];
+
+		// gets all links in the html document
+		$html.find('a').each(function(i, obj) {
+			var link = app.makeLink(url, $(this).attr('href'));
+
+			if(link != null) {
+				links.push(link);
+			}
+		});
+
+		return links;
+	}
+}
+
+try {
+	// Testing
+	module.exports = function(EightyAppBase) {
+		EightyApp.prototype = new EightyAppBase();
+		return new EightyApp();
+	}
+} catch(e) {
+	// Production
+	console.log("Eighty app exists.");
+	EightyApp.prototype = new EightyAppBase();
+}
