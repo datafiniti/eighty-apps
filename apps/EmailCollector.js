8167c37e3e040e78c2a77d620bdb41e7113cef3d
shiondev <shion@datafiniti.net>
2014-05-22 19:24:56 +0000

    adding EmailCollector

---
+++ apps/EmailCollector.js
@@ -0,0 +1,54 @@
+// Returns a list of emails for every page on the domains of the URL list
+
+var EightyApp = function() {
+	this.processDocument = function(html, url, headers, status, jQuery) {
+		var app = this;
+		$ = jQuery;
+		var $html = app.parseHtml(html, $);
+		var object = {};
+
+		// Get emails
+		var emailList = [];
+		emailList = html.match(/[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9]+)*(\.[a-z]{2,})/gi);
+		object.emailList = emailList;
+
+		return JSON.stringify(object);
+	}
+
+	this.parseLinks = function(html, url, headers, status, jQuery) {
+		var app = this;
+		var $ = jQuery;
+		var $html = app.parseHtml(html, $);
+		var links = [];
+
+		var r = /:\/\/(.[^/]+)/;
+		var urlDomain = url.match(r)[1]
+
+		// gets all links in the html document
+		$html.find('a').each(function(i, obj) {
+			// console.log($(this).attr('href'));
+			var link = app.makeLink(url, $(this).attr('href'));
+
+			if(link != null) {
+	                        var linkDomain = link.match(r)[1]
+				if (urlDomain == linkDomain) {
+					links.push(link);
+				}
+			}
+		});
+
+		return links;
+	}
+}
+
+try {
+	// Testing
+	module.exports = function(EightyAppBase) {
+		EightyApp.prototype = new EightyAppBase();
+		return new EightyApp();
+	}
+} catch(e) {
+	// Production
+	console.log("Eighty app exists.");
+	EightyApp.prototype = new EightyAppBase();
+}
