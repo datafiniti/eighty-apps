364232ba2120b0e30a2bc183b277ce2b96fa94ec
shiondev <shion@datafiniti.net>
2014-09-08 16:39:44 +0000

    adding headerdata app

---
+++ apps/HeaderData.js
@@ -0,0 +1,50 @@
+// This 80app returns the header data from each URL crawled
+
+var EightyApp = function() {
+        this.processDocument = function(html, url, headers, status, jQuery) {
+                var app = this;
+                var $ = jQuery;
+                var $html = app.parseHtml(html, $);
+                var object = {};
+
+		var headersArray = headers.split(\r\n);
+		for (var i = 0; i < headersArray.length; i++) {
+			var keyvalArray = headersArray[i].split(": ");
+			var key = keyvalArray[0];
+			var value = keyvalArray[1];
+			object[key] = value;
+		}
+
+                return JSON.stringify(object);
+        }
+
+        this.parseLinks = function(html, url, headers, status, jQuery) {
+                var app = this;
+                var $ = jQuery;
+                var $html = app.parseHtml(html, $);
+                var links = [];
+
+                // gets all links in the html document
+                $html.find('a').each(function(i, obj) {
+                        var link = app.makeLink(url, $(this).attr('href'));
+
+                        if(link != null) {
+                                links.push(link);
+                        }
+                });
+
+                return links;
+        }
+}
+
+try {
+        // Testing
+        module.exports = function(EightyAppBase) {
+                EightyApp.prototype = new EightyAppBase();
+                return new EightyApp();
+        }
+} catch(e) {
+        // Production
+        console.log("Eighty app exists.");
+        EightyApp.prototype = new EightyAppBase();
+}
