9a0f579f8a020dc1eda8f5fc1de7bdbbb237428f
shiondev <shion@datafiniti.net>
2014-12-14 01:59:23 +0000

    adding regexmatcher template

---
+++ apps/templates/RegexMatcher.js
@@ -0,0 +1,47 @@
+// This 80app returns all links found on a page
+
+var EightyApp = function() {
+	this.processDocument = function(html, url, headers, status, jQuery) {
+                var app = this;
+                var $ = jQuery;
+                var $html = app.parseHtml(html, $);
+		var object = {};
+
+                // gets all regex matches in the html document
+		var regexpattern = /80legs/;
+                var matches = html.match(regexpattern);
+		object.matches = matches;
+
+                return JSON.stringify(object);
+	}
+
+	this.parseLinks = function(html, url, headers, status, jQuery) {
+		var app = this;
+		var $ = jQuery;
+		var $html = app.parseHtml(html, $);
+		var links = [];
+
+		// gets all links in the html document
+		$html.find('a').each(function(i, obj) {
+			var link = app.makeLink(url, $(this).attr('href'));
+
+			if(link != null) {
+				links.push(link);
+			}
+		});
+
+		return links;
+	}
+}
+
+try {
+	// Testing
+	module.exports = function(EightyAppBase) {
+		EightyApp.prototype = new EightyAppBase();
+		return new EightyApp();
+	}
+} catch(e) {
+	// Production
+	console.log("Eighty app exists.");
+	EightyApp.prototype = new EightyAppBase();
+}
