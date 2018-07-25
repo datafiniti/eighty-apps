ba4405d6481b8061935e59c1d057c60baa67fdf5
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2018-07-25 18:54:13 -0500

    Bug fix / slight refactor in EmailCollector.js

--- apps/EmailCollector.js
+++ apps/EmailCollector.js
@@ -1,49 +1,51 @@
 // Returns a list of emails for every page on the domains of the URL list
+const EightyApp = require('eighty-app');
 
-var EightyApp = function() {
-	this.processDocument = function(html, url, headers, status, jQuery) {
-		var app = this;
-		$ = jQuery;
-		var $html = app.parseHtml(html, $);
-		var object = {};
+let app = new EightyApp();
 
-		// Get emails
-		var emailList = [];
-		emailList = html.match(/[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9]+)*(\.[a-z]{2,})/gi);
-		object.emailList = emailList;
+app.processDocument = function(html, url, headers, status, jQuery) {
+	$ = jQuery;
+	let $html = app.parseHtml(html, $);
+	let object = {};
 
-		return JSON.stringify(object);
+	// Get emails
+	let emailList = html.match(/[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9]+)*(\.[a-z]{2,})/gi);
+	
+	if (emailList.length >= 1) {
+	    emailList.shift();
+	    object = { emailList };
 	}
 
-	this.parseLinks = function(html, url, headers, status, jQuery) {
-		var app = this;
-		var $ = jQuery;
-		var $html = app.parseHtml(html, $);
-		var links = [];
-
-		var r = /:\/\/(.[^/]+)/;
-		var urlDomain = url.match(r)[1]
-
-		// gets all links in the html document
-		$html.find('a').each(function(i, obj) {
-			// console.log($(this).attr('href'));
-			var link = app.makeLink(url, $(this).attr('href'));
-
-			if (link != null) {
-                var linkDomain = link.match(r);
-                if (linkDomain && linkDomain.length > 1) {
-                    linkDomain = linkDomain[1];
-                if (urlDomain.toLowerCase() == linkDomain.toLowerCase())
-                    links.push(link);
-	            }
-			}
-		});
-
-		return links;
-	}
+    // It's possible that one page could contain the same email address multiple times, so we deduplify them.
+	return app.removeAllDuplicates(object);
 }
 
-module.exports = function (EightyAppBase) {
-	EightyApp.prototype = new EightyAppBase();
-	return new EightyApp();
+app.parseLinks = function(html, url, headers, status, jQuery) {
+	let $ = jQuery;
+	let $html = app.parseHtml(html, $);
+	let links = [];
+
+	let r = /:\/\/(.[^/]+)/;
+	let urlDomain = url.match(r)[1]
+
+	// gets all links in the html document
+	$html.find('a').each(function(i, obj) {
+		// console.log($(this).attr('href'));
+		let link = app.makeLink(url, $(this).attr('href'));
+
+		if (link != null) {
+            let linkDomain = link.match(r);
+            if (linkDomain && linkDomain.length > 1) {
+                linkDomain = linkDomain[1];
+            if (urlDomain.toLowerCase() == linkDomain.toLowerCase())
+                links.push(link);
+            }
+		}
+	});
+
+	return links;
+}
+
+module.exports = () => {
+	return app;
 }
