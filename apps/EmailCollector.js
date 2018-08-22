b072bd9b87e41ea4aad4029ce539f3014af71faa
Moe Jangda <m.jangda88@gmail.com>
2018-08-22 14:34:46 -0500

    fix EmailCollector 80app

--- apps/EmailCollector.js
+++ apps/EmailCollector.js
@@ -1,51 +1,49 @@
 // Returns a list of emails for every page on the domains of the URL list
 const EightyApp = require('eighty-app');
+const app = new EightyApp();
 
-let app = new EightyApp();
+const EMAIL_REGEX = /([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})/ig;
 
-app.processDocument = function(html, url, headers, status, jQuery) {
-	$ = jQuery;
-	let $html = app.parseHtml(html, $);
-	let object = {};
+app.processDocument = function(html) {
+    let data = {};
 
-	// Get emails
-	let emailList = html.match(/[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9]+)*(\.[a-z]{2,})/gi);
-	
-	if (emailList.length >= 1) {
-	    emailList.shift();
-	    object = { emailList };
-	}
+    // Get emails
+    const emailList = html.match(EMAIL_REGEX);
+
+    if (emailList) {
+        data.emails = emailList;
+    }
 
     // It's possible that one page could contain the same email address multiple times, so we deduplify them.
-	return app.removeAllDuplicates(object);
-}
+    return app.removeAllDuplicates(data);
+};
 
 app.parseLinks = function(html, url, headers, status, jQuery) {
-	let $ = jQuery;
-	let $html = app.parseHtml(html, $);
-	let links = [];
+    let $ = jQuery;
+    let $html = app.parseHtml(html, $);
+    let links = [];
 
-	let r = /:\/\/(.[^/]+)/;
-	let urlDomain = url.match(r)[1]
+    let r = /:\/\/(.[^/]+)/;
+    let urlDomain = url.match(r)[1];
 
-	// gets all links in the html document
-	$html.find('a').each(function(i, obj) {
-		// console.log($(this).attr('href'));
-		let link = app.makeLink(url, $(this).attr('href'));
+    // gets all links in the html document
+    $html.find('a').each(function(i, obj) {
+        // console.log($(this).attr('href'));
+        let link = app.makeLink(url, $(this).attr('href'));
 
-		if (link != null) {
+        if (link != null) {
             let linkDomain = link.match(r);
             if (linkDomain && linkDomain.length > 1) {
                 linkDomain = linkDomain[1];
-            if (urlDomain.toLowerCase() == linkDomain.toLowerCase())
-                links.push(link);
+                if (urlDomain.toLowerCase() == linkDomain.toLowerCase())
+                    links.push(link);
             }
-		}
-	});
+        }
+    });
 
-	return links;
-}
+    return links;
+};
 
 module.exports = () => {
-	return app;
-}
+    return app;
+};
