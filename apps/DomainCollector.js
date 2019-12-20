7a94a3f4b873ab53b829ab2a743dc00c2319b14e
Moe Jangda <m.jangda88@gmail.com>
2019-12-20 23:37:35 +0500

    fix

--- apps/DomainCollector.js
+++ apps/DomainCollector.js
@@ -1,63 +1,71 @@
+
 // This 80app returns the count of every domain linked from each URL crawled
 // The 80app will only crawl to links on the current domain.
 
 var EightyApp = function() {
-	this.processDocument = function(html, url, headers, status, jQuery) {
-                var app = this;
-                var $ = jQuery;
-                var $html = app.parseHtml(html, $);
-		var object = {};
+    this.processDocument = function(html, url, headers, status, jQuery) {
+        var app = this;
+        var $ = jQuery;
+        var $html = app.parseHtml(html, $);
+        var object = {};
 
-                // gets all links in the html document
-                var domainCount = {};
-		var r = /:\/\/(.[^/]+)/;
+        // gets all links in the html document
+        var domainCount = {};
+        var r = /:\/\/(.[^/]+)/;
 
-                $html.find('a').each(function(i, obj) {
-                        var link = app.makeLink(url, $(this).attr('href'));
-			if (link != null) {
-				var linkDomain = link.match(r);
-				if (linkDomain && linkDomain.length > 1)
-				    linkDomain = linkDomain[1];
-        	    if (linkDomain in domainCount)
-					domainCount[linkDomain] = domainCount[linkDomain] + 1;
-                else 
-					domainCount[linkDomain] = 1;
-			}
-                });
-		object.domainCount = domainCount;
+        $html.find('a').each(function(i, obj) {
+            var link = app.makeLink(url, $(this).attr('href'));
 
-                return JSON.stringify(object);
-	}
+            if (link != null) {
+                var linkDomain = link.match(r);
+                if (linkDomain && linkDomain.length > 1) {
+                    linkDomain = linkDomain[1];
+                }
 
-	this.parseLinks = function(html, url, headers, status, jQuery) {
-		var app = this;
-		var $ = jQuery;
-		var $html = app.parseHtml(html, $);
-		var links = [];
+                if (linkDomain in domainCount) {
+                    domainCount[linkDomain] = domainCount[linkDomain] + 1;
+                } else {
+                    domainCount[linkDomain] = 1;
+                }
+            }
+        });
+        object.domainCount = domainCount;
 
-		var r = /:\/\/(.[^/]+)/;
-		var urlDomain = url.match(r)[1]
+        return JSON.stringify(object);
+    };
 
-		// gets all links in the html document
-		$html.find('a').each(function(i, obj) {
-			// console.log($(this).attr('href'));
-			var link = app.makeLink(url, $(this).attr('href'));
+    this.parseLinks = function(html, url, headers, status, jQuery) {
+        var app = this;
+        var $ = jQuery;
+        var $html = app.parseHtml(html, $);
+        var links = [];
 
-			    if (link != null) {
-                    var linkDomain = link.match(r);
-                    if (linkDomain && linkDomain.length > 1) {
-                        linkDomain = linkDomain[1];
-                    if (urlDomain.toLowerCase() == linkDomain.toLowerCase())
-                        links.push(link);
-	               }
-			    }
-		});
+        var r = /:\/\/(.[^/]+)/;
+        var urlDomain = url.match(r)[1];
 
-		return links;
-	}
-}
+        // gets all links in the html document
+        $html.find('a').each(function(i, obj) {
+            // console.log($(this).attr('href'));
+            var link = app.makeLink(url, $(this).attr('href'));
+
+            if (link != null) {
+                var linkDomain = link.match(r);
+
+                if (linkDomain && linkDomain.length > 1) {
+                    linkDomain = linkDomain[1];
+                }
+
+                if (urlDomain.toLowerCase() == linkDomain.toLowerCase()) {
+                    links.push(link);
+                }
+            }
+        });
+
+        return links;
+    };
+};
 
 module.exports = function (EightyAppBase) {
-	EightyApp.prototype = new EightyAppBase();
-	return new EightyApp();
-}
+    EightyApp.prototype = new EightyAppBase();
+    return new EightyApp();
+};
