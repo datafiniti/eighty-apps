6a44c1155b2a0e8fba57ab73ea367dd1b1bcd3bf
David Poulos <davidpoulos@ymail.com>
2020-02-06 14:16:48 -0600

    Update LinkCollector.js

--- apps/LinkCollector.js
+++ apps/LinkCollector.js
@@ -1,50 +1,40 @@
-// This 80app returns all links found on a page
+const EightyApp = require('eighty-app');
+const app = new EightyApp();
 
-var EightyApp = function() {
-	this.processDocument = function(html, url, headers, status, jQuery) {
-                var app = this;
-                var $ = jQuery;
-                var $html = app.parseHtml(html, $);
-		var object = {};
 
-                // gets all links in the html document
-                var links = [];
-                $html.find('a').each(function(i, obj) {
-                        var link = app.makeLink(url, $(this).attr('href'));
-			var text = $(this).text();
-			var linkObject = {};
-			linkObject.link = link;
-			linkObject.text = text;
+app.processDocument = function(html, url, headers, status, cheerio, extras) {
+        const $ = cheerio;
+        const $html = app.parseHtml(html, cheerio);
+		const data = {};
+         // gets all links in the html document
+        var links = [];
+        $html.find('a').each(function(i, obj) {
+            var link = app.makeLink(url, $(this).attr('href'));
+    		var text = $(this).text();
+    		var linkObject = {};
+    		linkObject.link = link;
+    		linkObject.text = text;
+                if (link != null) {
+    			    links.push(linkObject);
+                }
+        });
+    data.links = links;
+    return JSON.stringify(data);
+};
 
-                        if (link != null) {
-				links.push(linkObject);
-                        }
-                });
-		object.links = links;
-
-                return JSON.stringify(object);
-	}
-
-	this.parseLinks = function(html, url, headers, status, jQuery) {
-		var app = this;
-		var $ = jQuery;
-		var $html = app.parseHtml(html, $);
-		var links = [];
-
+app.parseLinks = function(html, url, headers, status, cheerio, extras) {
+		const $ = cheerio;
+		const $html = app.parseHtml(html, $);
+		const links = [];
 		// gets all links in the html document
 		$html.find('a').each(function(i, obj) {
-			var link = app.makeLink(url, $(this).attr('href'));
-
+			const link = app.makeLink(url, $(this).attr('href'));
 			if(link != null) {
 				links.push(link);
 			}
 		});
-
 		return links;
-	}
 }
-
-module.exports = function (EightyAppBase) {
-	EightyApp.prototype = new EightyAppBase();
-	return new EightyApp();
-}
+module.exports = function() { 
+    return app;
+};
